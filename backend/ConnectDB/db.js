5f22adbdfe3af6c337b6a2abe77c1ca41d9039fa
NAVEENKRISHNA-KR <naveenkrishnakr.21cse@kongu.edu>
2024-05-15 06:48:15 +0530

    dsfev

--- backend/ConnectDB/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = (url)=>{
-   return mongoose.connect(url)
-    .then(() => console.log("Connected to DB"))
-    .catch (console.error);
-}
-
-module.exports = connectDB;
--- backend/Middlewares/notFoundURL.js
+++
@@ -1,5 +0,0 @@
-const notFoundURL = async(req,res) => {
-    return res.status(500).send({ message: "URL not found" });
-}
-
-module.exports = notFoundURL;
